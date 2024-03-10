65887cef025c45d499e23c3c43caff9375039cc4
kavyanshkumar18@gmail.com <kavyanshkumar18@gmail.com>
2024-03-10 22:04:56 +0530

    custom router

---
+++ 06reactRouter/src/components/Footer/Footer.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom';
+
+export default function Footer() {
+    return (
+        <footer className="bg-white border-y">
+            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+                <div className="md:flex md:justify-between">
+                    <div className="mb-6 md:mb-0">
+                        <Link to="/" className="flex items-center">
+                            <img
+                                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                                className="mr-3 h-16"
+                                alt="Logo"
+                            />
+                        </Link>
+                    </div>
+                    <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
+                            <ul className="text-gray-500 font-medium">
+                                <li className="mb-4">
+                                    <NavLink to="/" className={({isActive}) => 
+                                  `hover:underline ${isActive ? "text-orange-700" : "text-gray-700"}`}>
+                                        Home
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/about" className={({isActive}) => 
+                                  `hover:underline ${isActive ? "text-orange-700" : "text-gray-700"}`}>
+                                        About
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow us</h2>
+                            <ul className="text-gray-500 font-medium">
+                                <li className="mb-4">
+                                    <a
+                                        href="https://github.com/hiteshchoudhary"
+                                        className="hover:underline"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Github
+                                    </a>
+                                </li>
+                                <li>
+                                    <Link to="/" className="hover:underline">
+                                        Discord
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
+                            <ul className="text-gray-500 font-medium">
+                                <li className="mb-4">
+                                    <Link to="#" className="hover:underline">
+                                        Privacy Policy
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="#" className="hover:underline">
+                                        Terms &amp; Conditions
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                 </div>
+        </footer>
+    );
+}
